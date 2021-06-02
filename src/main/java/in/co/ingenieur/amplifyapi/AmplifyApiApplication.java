e324ab0755a857e7310ac752c8c73b2f68911545
viveksonkar <viveksonkar19n@gmail.com>
2021-06-02 21:21:03 +0200

    Initial Commit

---
+++ src/main/java/in/co/ingenieur/amplifyapi/AmplifyApiApplication.java
@@ -0,0 +1,13 @@
+package in.co.ingenieur.amplifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmplifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/ingenieur/amplifyapi/AmplifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.ingenieur.amplifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
